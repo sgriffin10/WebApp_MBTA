c4f24ffdab1b85eab632fb39ccb88e441cb904dd
sgriffin10 <sgriffin4@babson.edu>
2019-11-11 21:23:59 -0500

    added seperate api file

---
+++ api_credentials.py
@@ -0,0 +1,7 @@
+# Useful URLs (you need to add the appropriate parameters for your requests)
+MAPQUEST_BASE_URL = "http://open.mapquestapi.com/geocoding/v1/address"
+MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
+
+# Your API KEYS (you need to use your own keys - very long random characters)
+MAPQUEST_API_KEY = ""
+MBTA_API_KEY = ""
