98b160b6f169eb8cc3e912e72635a36d339fc294
ecedano1 <54921157+ecedano1@users.noreply.github.com>
2019-11-13 21:30:54 -0500

    up

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
-"""
-Belongs to Branch 2
-Simple "Hello, World" application using Flask
-"""
+# """
+# Belongs to Branch 2
+# Simple "Hello, World" application using Flask
+# """
 
-from flask import Flask
+# from flask import Flask
 
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def homepage():
+#     return render_template("homepage.html")
+
+
+# @mbta_helper.route("/hello/")
+# @mbta_helper.route("/hello/<name>")
+# def hello(name=None):
+#     if name:
+#         name = name.upper()
+#     return render_template("hello.html", name=name)
+
+
+from flask import Flask, escape, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'index'
 
-@app.route("/")
-def homepage():
-    return render_template("homepage.html")
+@app.route('/login')
+def login():
+    return 'login'
 
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    if name:
-        name = name.upper()
-    return render_template("hello.html", name=name)
-
-
-
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
